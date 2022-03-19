644863a5b297be1fb48f269490a8074a975a5eb2
CJ5467 <84459784+CJ5467@users.noreply.github.com>
2022-03-19 15:35:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ let players = [];
 
 //Create db connection
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    host: 127.0.0.1,
+    port: 3307,
+    user: newuser,
+    password: password,
+    database: sampleapp
 });
 
 //Connect
