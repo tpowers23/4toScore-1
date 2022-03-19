bce3843eea262e9f7a09b5b81df092053b1a835b
CJ5467 <84459784+CJ5467@users.noreply.github.com>
2022-03-19 12:53:08 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+let players = [];
+
+//Create db connection
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+//Connect
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('MySql connected..');
+})
+
+// EJS
+app.set('view engine', 'ejs');
+
+//middleware to read req.body.<params>
+app.use(express.json())
+
+//Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+//static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+app.get('/unranked', (req, res) => {
+    res.render('unranked.ejs')
+})
+
+app.get('/howtoplay', (req, res) => {
+    res.render('howtoplay.ejs')
+})
+
+app.get('/eloranking', (req, res) => {
+    res.render('eloranking.ejs')
+})
+
+app.get('/aboutus', (req, res) => {
+    res.render('aboutus.ejs')
+})
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
+
+//socket io setup 
+io.on('connection', function (socket)  {
+	console.log('A user connected:' + socket.id);
+    players.push(socket.id);
+    let coinFlip = Math.floor(Math.random() * 2);
+    if (players.length === 1) {
+        io.emit('isPlayerA');
+    }
+    if (players.length === 2){
+        io.emit('whoseTurn', coinFlip);
+    }
+    socket.on('diskDropped', function(moveCol, isPlayerA){
+        io.emit('moveMade', moveCol, isPlayerA);
+    });
+    socket.on('gameOver', (winner) => {
+        io.emit('gameOver', winner);
+    });
+    socket.on('disconnect', () => {
+          console.log('A user disconnected:' + socket.id);
+          players = players.filter(player => player !== socket.id);
+    });
+});
+
+
+//const PORT = process.env.PORT || 5001;
+//app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+server.listen(5001, () => {
+    console.log(`Listening on ${server.address().port}`);
+});
---
+++ controllers/auth.js
@@ -0,0 +1,84 @@
+const mysql=require('mysql');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config({path: './.env'});
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+//CREATE USER i.e. Register
+exports.register = async (req, res) => {
+    const { fname, lname, email, username, password, passwordConfirm } = req.body;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await db.query('SELECT user_email FROM players WHERE user_email =?', [email], async (err, results) => {
+        if (err) throw err;
+        console.log("-----> Search Results")
+        console.log(results.length)
+        if (results.length != 0) {
+            console.log("-----> Email already in use")
+            message="Email already in use"
+            res.render('register.ejs')
+            //res.sendStatus(409)
+        }
+        else if(password.length<8 || password.search(/[!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~]/) < 0 || password.search(/[A-Z]/) < 0){
+            message="Password needs to be at least 8 characters, have an uppercase letter, & include a special symbol like @, !, ?"
+            res.render('register.ejs')
+        }
+        else if(password!=passwordConfirm){
+            console.log("-----> Passwords do not match")
+            message="Passwords do not match"
+            res.render('register.ejs')
+        }
+        else {
+            await db.query('INSERT INTO players SET ?', {
+                user_firstname: fname, user_lastname: lname,
+                user_email: email, username: username, user_password: hashedPassword
+            },
+                (err, results) => {
+                    if (err) throw err;
+                    console.log("-------> Created New user")
+                    console.log(results.insertId)
+                    res.redirect('/login')
+                    //res.sendStatus(201)
+                })
+        }
+    })// end of connection.query()
+} //end of db.getConnection()
+// end of app.post()
+
+//LOGIN
+exports.login = async( req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    await db.query('SELECT * FROM players WHERE username =?', [username], async (err, results) => {
+        if (err) throw err;
+        console.log("-----> Search Results")
+        
+        console.log(results.length)
+
+        if (results.length == 0) {
+            console.log("-----> User does not exist")
+            message="User does not exist"
+            res.render('login')
+        }
+        else {
+            const hashedPassword = results[0].user_password
+            //get the hashedpassword from result
+            if(await bcrypt.compare(password, hashedPassword)){
+                console.log('-------> Login Successful')
+                res.redirect('/')
+            }
+            else {
+                console.log('-------> Password incorrect')
+                message="Password Incorrect"
+                res.render('login.ejs');
+            } // end of bcrypt.compare()
+        }// end of user exists 
+    })// end of connection
+}
