5e250f541c265f28ae10954e4fd178d69a4962a0
tpowers23 <63263308+tpowers23@users.noreply.github.com>
2022-03-22 16:57:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,11 +9,10 @@ let players = [];
 
 //Create db connection
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'ba9e9a435785aa',
+    password: '3baf3826',
+    database: 'heroku_14a186278551e7b'
 });
 
 //Connect
