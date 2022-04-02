90926a070fd090299793657c51fe4d524b305461
CJ5467 <84459784+CJ5467@users.noreply.github.com>
2022-04-02 16:49:30 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,9 +14,6 @@ var crypto = require('crypto');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-//get url
-//let path1 = window.location.pathname;
-//console.log(path1);
 
 // create variables and object for rooms
 let players = [];
@@ -66,6 +63,9 @@ initWebRoutes(app);
 io.on('connection', async (socket) =>  {
 
 	console.log('A user connected:' + socket.id);
+	//get url
+	let path1 = window.location.pathname;
+	console.log(path1);
     // if there are no rooms available to join
     // IF (current URL points to /unranked)
         if (openRooms.length < 1){
